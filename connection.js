a763190c812ad598c072fa1c1c17c900c5586707
AbhishekKasera7696 <akasera62@gmail.com>
2022-05-14 14:59:32 +0530

    make it better

---
+++ connection.js
@@ -0,0 +1,20 @@
+// const mongoose = require("mongoose");
+// require('dotenv').config();
+
+// mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.z2g95.mongodb.net/chatApp?retryWrites=true&w=majority`, () => {
+//     console.log("connected to mongodb")
+// })
+
+
+
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.60zdu.mongodb.net/mychatapp?retryWrites=true&w=majority`, () => {
+    console.log("Connected to mongoDB");
+});
+
+
+
+
